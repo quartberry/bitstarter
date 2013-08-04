3de540faf049bb00e129bad76e4807635c13f5bc
Mark Pazolli <pazollim@gmail.com>
2013-08-04 12:27:49 +0800

    Correct port for heroku

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var util = require('util'),
 connect = require('connect'),
-port = 8080;
+port = process.env.PORT || 5000;
 
 connect.createServer(connect.static(__dirname + '/web/')).listen(port);
 util.puts('Listening on ' + port + '...');
